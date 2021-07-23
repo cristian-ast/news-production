9d7ef77a84a74a06fb92cc8f7fd9570623422671
Cristian Castro Astacio <cristiancastroastacio@gmail.com>
2021-07-23 15:27:21 -0400

    Syntax error fixed new --> New

--- components/News.js
+++ components/News.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import New from './new';
+import New from './New';
 import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
 
 const News = ({data, tipo}) => {
